f747ad213fb0e6a1d9573bdf7c531e72f4b4f4c6
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2019-11-24 14:36:13 -0300

    tab pascal

--- Main.pas
+++ Main.pas
@@ -3,10 +3,25 @@ unit Main;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.TabControl,
   FMX.StdCtrls, FMX.Gestures, FMX.Controls.Presentation, FMX.Objects,
-  FMX.Layouts, FMX.Edit;
+  FMX.Layouts, FMX.Edit, FMX.ScrollBox, FMX.Memo,
+
+  REST.Types,
+  REST.Client,
+  Web.HTTPApp,
+  System.JSON,
+  System.Threading,
+
+{$IFDEF ANDROID}
+  Androidapi.JNI.GraphicsContentViewText, Androidapi.Helpers,
+  Androidapi.JNI.Telephony, Androidapi.JNI.Provider, Androidapi.JNIBridge,
+  Androidapi.JNI.JavaTypes, Androidapi.JNI.Os,
+  Androidapi.JNI.App, Data.Bind.Components, Data.Bind.ObjectScope,
+  System.ImageList, FMX.ImgList
+{$ENDIF};
 
 type
   TTabbedForm = class(TForm)
@@ -27,11 +42,29 @@ type
     Layout2: TLayout;
     Label3: TLabel;
     Edit3: TEdit;
+    Layout3: TLayout;
+    Rectangle2: TRectangle;
+    EditButton1: TEditButton;
+    EditButton2: TEditButton;
+    EditButton3: TEditButton;
+    GridPanelLayout2: TGridPanelLayout;
+    Edit4: TEdit;
+    Memo1: TMemo;
+    EditButton4: TEditButton;
     procedure FormCreate(Sender: TObject);
     procedure FormGesture(Sender: TObject; const EventInfo: TGestureEventInfo;
       var Handled: Boolean);
+    procedure EditButton2Click(Sender: TObject);
+    procedure EditButton4Click(Sender: TObject);
+
+
+    procedure SendTextToDialogFlow();
   private
     { Private declarations }
+    fOAuthID: string;
+    fProjectID: string;
+    fSessionID: string;
+
   public
     { Public declarations }
   end;
@@ -42,11 +75,24 @@ var
 implementation
 
 {$R *.fmx}
+{$R *.LgXhdpiPh.fmx ANDROID}
+
+procedure TTabbedForm.EditButton2Click(Sender: TObject);
+begin
+  fOAuthID := Edit1.Text
+end;
+
+procedure TTabbedForm.EditButton4Click(Sender: TObject);
+begin
+  // Memo1.Lines.Add(fOAuthID);
+
+end;
 
 procedure TTabbedForm.FormCreate(Sender: TObject);
 begin
   { This defines the default active tab at runtime }
   TabControl1.ActiveTab := TabItem1;
+
 end;
 
 procedure TTabbedForm.FormGesture(Sender: TObject;
@@ -55,18 +101,19 @@ begin
 {$IFDEF ANDROID}
   case EventInfo.GestureID of
     sgiLeft:
-    begin
-      if TabControl1.ActiveTab <> TabControl1.Tabs[TabControl1.TabCount-1] then
-        TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex+1];
-      Handled := True;
-    end;
+      begin
+        if TabControl1.ActiveTab <> TabControl1.Tabs[TabControl1.TabCount - 1]
+        then
+          TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex + 1];
+        Handled := True;
+      end;
 
     sgiRight:
-    begin
-      if TabControl1.ActiveTab <> TabControl1.Tabs[0] then
-        TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex-1];
-      Handled := True;
-    end;
+      begin
+        if TabControl1.ActiveTab <> TabControl1.Tabs[0] then
+          TabControl1.ActiveTab := TabControl1.Tabs[TabControl1.TabIndex - 1];
+        Handled := True;
+      end;
   end;
 {$ENDIF}
 end;
